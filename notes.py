d04dc425972c764c06f3c2d9dd9394ab0d606f36
catherineuhs <cliffordcat@urbandaleschools.com>
2020-03-03 14:23:34 -0600

    commit message

--- notes.py
+++ notes.py
@@ -1,24 +1,39 @@
 import arcade
 SW=640
 SH=480
+class Ball:
+    def __init__(self,pos_x,pos_y, dx,dy, rad, color):
+        self.pos_x=pos_x
+        self.pos_y=pos_y
+        self.dx=dx
+        self.dy=dy
+        self.rad=rad
+        self.color=color
+    def draw_ball(self):
+        arcade.draw_circle_filled(self.pos_x, self.pos_y, self.rad, self.color)
 
+
+    def update_ball(self):
+        self.pos_x+=self.dx
+        self.pos_y+=self.dy
 class MyGame(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
-        arcade.set_background_color(arcade.color.BUBBLES)
-        self.x = SW/2
-        self.y = SH/2
+        arcade.set_background_color(arcade.color.WHITE)
+        self.ball=Ball(SW/2,SH/2,3,-2,15, arcade.color.APPLE_GREEN)
 
     def on_draw(self): #going to be running 60x per second
         arcade.start_render()
-        arcade.draw_lrtb_rectangle_filled()
-
+        self.ball.draw_ball()
+    def on_update(self, dt):
+        self.x-=3
+        self.y-=3
 
 
 
 
 def main():
-    window=arcade.Window(SW, SH, "Animation")
+    window=MyGame(SW, SH, "Animation")
     arcade.run()
 
 
